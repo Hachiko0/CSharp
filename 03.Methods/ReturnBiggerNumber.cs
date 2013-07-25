742a58c96bcd6d2ecc5bbd57a47c9858272b058c
Hachiko <shegobiec60@gmail.com>
2013-07-25 13:53:06 +0300

    Methods

---
+++ 03.Methods/ReturnBiggerNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+/*Write a method GetMax() with two parameters 
+ * that returns the bigger of two integers. Write 
+ * a program that reads 3 integers from the console
+ * and prints the biggest of them using the method GetMax(). */
+
+class ReturnBiggerNumber
+{
+    static int GetMax(int firstNumber, int secondNumber)
+    {
+        if (firstNumber > secondNumber)
+        {
+            return firstNumber;
+        }
+        else return secondNumber;
+        //we dont check if they are equal because the task does not mention they can be equal
+    }
+    static void Main()
+    {
+        int first = int.Parse(Console.ReadLine());
+        int second = int.Parse(Console.ReadLine());
+        int third = int.Parse(Console.ReadLine());
+        int biggest = GetMax(GetMax(first, second), third);
+        Console.Write("The biggest number is : ");
+        Console.WriteLine(biggest);
+    }
+}
+
---
+++ 03.Methods/ReturnLastDigitInEnglish.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/* Write a method that returns the last digit 
+ * of given integer as an English word. Examples: 
+ * 512  "two", 1024  "four", 12309  "nine". */
+
+class ReturnLastDigitInEnglish
+{
+    static string LastDigitIntoWord(int number)
+    {
+        string[] digits = new string[10] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+        int lastDigit = number % 10;
+        return digits[lastDigit];
+    }
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine("Last digit : {0}",LastDigitIntoWord(n));
+    }
+}
+
